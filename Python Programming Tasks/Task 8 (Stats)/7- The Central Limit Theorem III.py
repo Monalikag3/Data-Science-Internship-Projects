f5070dd92347d20579ec126fe51da2e067aa1853
Monalika Gupta <98216699+Monalikag3@users.noreply.github.com>
2022-08-21 17:09:41 +0530

    Add files via upload

---
+++ Python Programming Tasks/Task 8 (Stats)/7- The Central Limit Theorem III.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+mean = 500
+std = 80
+n = 100
+z = 1.96
+
+mean = mean
+std = std / n**(1/2)
+
+print(round(mean - std * z, 2))
+print(round(mean + std * z, 2))
+
